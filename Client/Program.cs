c296e42db85ce2bd7aba2b01dbc36d2d51195146
TalonEzio <vuthemanh1707@gmail.com>
2023-12-10 09:46:16 +0700

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        static int port = 10000;
+        static IPAddress serverIpAddress = IPAddress.Parse("127.0.0.1");
+        static bool stop = false;
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+
+            var thread = new Thread(Input);
+
+            thread.Start();
+            while(true)
+            {
+                if (stop)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Bam phim bat ki de ket thuc chuong trinh");
+            Console.ReadLine();
+            
+        }
+        static void Input()
+        {
+           while(true)
+            {
+                Console.Write("Ky tu: ");
+                var key = Console.ReadKey();
+
+                Console.WriteLine();
+                SendToServer(key.KeyChar);
+                
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    stop = true;
+                    return;
+                }
+
+                //Console.WriteLine($"\nKy tu vua nhap: {key.KeyChar}");
+
+            }
+        }
+
+        private static void SendToServer(char keyChar)
+        {
+            var client = new UdpClient();
+            
+            client.Connect(serverIpAddress, port);
+
+            var sendByte = Encoding.ASCII.GetBytes(keyChar.ToString());
+
+            client.Send(sendByte,sendByte.Length);
+
+            Console.WriteLine("Gui du lieu thanh cong");
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Threading;
+namespace Server
+{
+    internal class Program
+    {
+        static int port = 10000;
+        static IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        static bool stop = false;
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+
+            var thread = new Thread(Output);
+            thread.Start();
+            Console.WriteLine("Server san sang!");
+            while (true)
+            {
+                if (stop)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Bam phim bat ki de ket thuc chuong trinh");
+            Console.ReadLine();
+        }
+        static void Output()
+        {
+            var server = new UdpClient(new IPEndPoint(ipAddress, port));
+            while (true)
+            {
+                var dummy = new IPEndPoint(0, 0);
+
+                var buffer = server.Receive(ref dummy);
+                Console.WriteLine($"Nhan duoc du lieu tu {dummy}");
+                char key = (char)buffer[0];
+                if (key == (char)ConsoleKey.Escape)
+                {
+                    stop = true;
+                    return;
+                }
+                Console.WriteLine($"Ky tu vua nhap: {key}");
+
+
+
+            }
+        }
+    }
+}
